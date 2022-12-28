97b67a1a1e3bd6435f7a9328f8c8a841c45920ac
Dmitry Nesterov <media.dn11@gmail.com>
2022-12-28 13:59:24 +0300

    add Eslint, export defaul in cli.js, Makefile: add eslint

--- src/cli.js
+++ src/cli.js
@@ -1,10 +1,9 @@
 import readlineSync from 'readline-sync';
 
 const userGreeting = () => {
-    console.log('Welcome to the Brain Games!');
-    const userName = readlineSync.question('May I have your name? ');
-    console.log(`Hello, ${userName}!`);
+  console.log('Welcome to the Brain Games!');
+  const userName = readlineSync.question('May I have your name? ');
+  console.log(`Hello, ${userName}!`);
 };
 
-export { userGreeting };
-
+export default userGreeting;
