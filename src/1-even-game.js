c7bf8fb358dd0288448d743dca92ed16ea63a961
Dmitry Nesterov <media.dn11@gmail.com>
2022-12-29 13:34:33 +0300

    fix 1-even-game.js

--- src/1-even-game.js
+++ src/1-even-game.js
@@ -24,7 +24,7 @@ const playingEvenGame = () => {
     }
   }
 
-  return console.log(`Congratulations, ${userName}!`);
+  return console.log(`, ${userName}!`);
 };
 
 export default playingEvenGame;
